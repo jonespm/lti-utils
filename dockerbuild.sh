85c1efb74c8f4e28f7cbd00e7dc290454bebc99b
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2018-10-23 14:31:13 -0400

    Fixing up poms so this can be released to maven central fixes #5

---
+++ dockerbuild.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+case "$1" in
+'install')
+    echo "Installing via docker"
+
+    # This command runs docker, it mounts local directories /.m2 to improve recompilation, .gnupg for release and the local directory as the app directory
+    # This needs to use the "non-slim" maven docker build that includes gpg
+    docker run --rm -it --name lti-utils-build \
+    -e "MAVEN_OPTS= -XX:+TieredCompilation -XX:TieredStopAtLevel=1" \
+    -v "${HOME}"/.m2:/root/.m2 \
+    -v "${HOME}"/.gnupg:/root/.gnupg \
+    -v "${PWD}:/usr/src/app" \
+    -w /usr/src/app maven:3.5.4-jdk-8 \
+    /bin/bash -c "mvn -T 1C -B install" 
+;;
+'release')
+    echo "Releasing via Docker"
+    # This command runs docker, it mounts local directories /.m2 to improve recompilation, .gnupg for release and the local directory as the app directory
+    # This needs to use the "non-slim" maven docker build that includes gpg
+    docker run --rm -it --name lti-utils-build \
+    -e "MAVEN_OPTS= -XX:+TieredCompilation -XX:TieredStopAtLevel=1" \
+    -v "${HOME}"/.m2:/root/.m2 \
+    -v "${HOME}"/.gnupg:/root/.gnupg \
+    -v "${PWD}:/usr/src/app" \
+    -w /usr/src/app maven:3.5.4-jdk-8 \
+    /bin/bash -c "mvn -T 1C -B release:clean release:prepare && mvn -T 1C -B release:perform" 
+;;
+*)
+echo "Usage: $0 [install|release]"
+;;
+esac
