c27180d1e904d563428acf022a02e2f1f7b6254d
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2018-10-23 15:37:54 -0400

    Updating dockerbuild for ssh-agent

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -14,18 +14,24 @@ case "$1" in
     -w /usr/src/app maven:3.5.4-jdk-8 \
     /bin/bash -c "mvn -T 1C -B install" 
 ;;
+'agent')
+    # This seems to be necessary to setup SSH for github release
+    # https://github.com/nardeas/docker-ssh-agent
+    docker run -d --name=ssh-agent nardeas/ssh-agent
+    docker run --rm --volumes-from=ssh-agent -v ~/.ssh:/.ssh -it nardeas/ssh-agent ssh-add /root/.ssh/id_rsa
+
+;;
 'release')
     echo "Releasing via Docker"
     # This command runs docker, it mounts local directories /.m2 to improve recompilation, .gnupg for release and the local directory as the app directory
     # This needs to use the "non-slim" maven docker build that includes gpg
     docker run --rm -it --name lti-utils-build \
+    --volumes-from=ssh-agent \
+    -e SSH_AUTH_SOCK=/.ssh-agent/socket \
     -e "MAVEN_OPTS= -XX:+TieredCompilation -XX:TieredStopAtLevel=1" \
     -v "${HOME}"/.m2:/root/.m2 \
     -v "${HOME}"/.gitconfig:/root/.gitconfig \
-    -v "${HOME}"/.ssh:/root/.ssh \
     -v "${HOME}"/.gnupg:/root/.gnupg \
-    -v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) \
-    -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
     -v "${PWD}:/usr/src/app" \
     -w /usr/src/app maven:3.5.4-jdk-8 \
     /bin/bash -c "mvn release:clean release:prepare && mvn release:perform" 
@@ -35,6 +41,8 @@ case "$1" in
     # This command runs docker, it mounts local directories /.m2 to improve recompilation, .gnupg for release and the local directory as the app directory
     # This needs to use the "non-slim" maven docker build that includes gpg
     docker run --rm -it --name lti-utils-build \
+    --volumes-from=ssh-agent \
+    -e SSH_AUTH_SOCK=/.ssh-agent/socket \
     -e "MAVEN_OPTS= -XX:+TieredCompilation -XX:TieredStopAtLevel=1" \
     -v "${HOME}"/.m2:/root/.m2 \
     -v "${HOME}"/.gitconfig:/root/.gitconfig \
