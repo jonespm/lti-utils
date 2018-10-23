87de8cf9f81943a240523310f11f32e3eb71db26
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2018-10-23 14:45:02 -0400

    Updating dockerbuild to include gitconfig

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -21,10 +21,26 @@ case "$1" in
     docker run --rm -it --name lti-utils-build \
     -e "MAVEN_OPTS= -XX:+TieredCompilation -XX:TieredStopAtLevel=1" \
     -v "${HOME}"/.m2:/root/.m2 \
+    -v "${HOME}"/.gitconfig:/root/.gitconfig \
+    -v "${HOME}"/.ssh:/root/.ssh \
     -v "${HOME}"/.gnupg:/root/.gnupg \
     -v "${PWD}:/usr/src/app" \
     -w /usr/src/app maven:3.5.4-jdk-8 \
     /bin/bash -c "mvn release:clean release:prepare && mvn release:perform" 
+;;
+'rollback')
+    echo "Rollback via docker"
+    # This command runs docker, it mounts local directories /.m2 to improve recompilation, .gnupg for release and the local directory as the app directory
+    # This needs to use the "non-slim" maven docker build that includes gpg
+    docker run --rm -it --name lti-utils-build \
+    -e "MAVEN_OPTS= -XX:+TieredCompilation -XX:TieredStopAtLevel=1" \
+    -v "${HOME}"/.m2:/root/.m2 \
+    -v "${HOME}"/.gitconfig:/root/.gitconfig \
+    -v "${HOME}"/.ssh:/root/.ssh \
+    -v "${HOME}"/.gnupg:/root/.gnupg \
+    -v "${PWD}:/usr/src/app" \
+    -w /usr/src/app maven:3.5.4-jdk-8 \
+    /bin/bash -c "mvn release:rollback" 
 ;;
 *)
 echo "Usage: $0 [install|release]"
